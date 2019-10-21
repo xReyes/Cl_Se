1d7bd347a98c2ef8335b40849d19433a585455bd
Prototipo-sco <45570616+Prototipo-sco@users.noreply.github.com>
2019-10-21 09:09:54 -0500

    Seguros Terminado

--- src/Conexion.java
+++ src/Conexion.java
@@ -12,9 +12,9 @@ public class Conexion {
     public Connection Conexion() {
         try {
             DriverManager.registerDriver(new org.gjt.mm.mysql.Driver());
-//            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/sistema_bancario?characterEncoding=latin1&useConfigs=maxPerformance", "root", "");
-            Connection con = DriverManager.getConnection("jdbc:mysql://sql9.freemysqlhosting.net/sql9307921", "sql9307921", "KTZ1jVIB18");
-            // con.createStatement();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost/sistema_bancario?characterEncoding=latin1&useConfigs=maxPerformance", "root", "");
+//            Connection con = DriverManager.getConnection("jdbc:mysql://sql9.freemysqlhosting.net/sql9307921", "sql9307921", "KTZ1jVIB18");
+            con.createStatement();
             this.conn = con;
         } catch (SQLException e) {
             //e.printStackTrace();
